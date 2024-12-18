a3be1cec56c00e9bdd5797d92ef33ca29b88c4c3
Akshat-Shah04 <shahakshat200@gmail.com>
2024-12-19 00:25:27 +0530

    Update File

--- myenv2/myproject2/myproject2/wsgi.py
+++ myenv2/myproject2/myproject2/wsgi.py
@@ -1,6 +1,8 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject2.settings')
+sys.path.append(os.path.join(os.path.dirname(__file__), "myproject2"))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject2.settings")
 
 application = get_wsgi_application()
