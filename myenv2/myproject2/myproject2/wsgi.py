857a8f3c3c37e3ba6711eb1dcbb5b7f8dbd8beb6
Akshat-Shah04 <shahakshat200@gmail.com>
2024-12-18 23:33:20 +0530

    Deployment

--- myenv2/myproject2/myproject2/wsgi.py
+++ myenv2/myproject2/myproject2/wsgi.py
@@ -8,22 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject2.settings')
 
-# assuming your django settings file is at '/home/AkshatShah/mysite/mysite/settings.py'
-# and your manage.py is is at '/home/AkshatShah/mysite/manage.py'
-path = '/C:/Users/pathi/Desktop/Program/Tops/BackEnd/Django/myenv2/myproject2'
-if path not in sys.path:
-
-   sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
-
-# then:
 application = get_wsgi_application()
-
-
